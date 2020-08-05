c1d183824bb64cccf0f4fb3908aec4181ff966c9
unknown <thdominguez5@gmail.com>
2020-08-05 21:11:21 +0200

    version 1

---
+++ src/main/java/com/calendarfx/app/CalendarApp.java
@@ -0,0 +1,107 @@
+/*
+ *  Copyright (C) 2017 Dirk Lemmermann Software & Consulting (dlsc.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.calendarfx.app;
+
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.Calendar.Style;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.view.CalendarView;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class CalendarApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        CalendarView calendarView = new CalendarView();
+
+        Calendar tomi = new Calendar("tomi");
+        Calendar javi = new Calendar("javi");
+        Calendar manolo = new Calendar("manolo");
+        Calendar antonio = new Calendar("antonio");
+        Calendar raul = new Calendar("raul");
+        Calendar birthdays = new Calendar("Cumpleaños");
+        Calendar holidays = new Calendar("Vacaciones");
+
+        tomi.setShortName("K");
+        javi.setShortName("D");
+        manolo.setShortName("P");
+        antonio.setShortName("J");
+        raul.setShortName("A");
+        birthdays.setShortName("B");
+        holidays.setShortName("H");
+
+        tomi.setStyle(Style.STYLE1);
+        javi.setStyle(Style.STYLE2);
+        manolo.setStyle(Style.STYLE3);
+        antonio.setStyle(Style.STYLE4);
+        raul.setStyle(Style.STYLE5);
+        birthdays.setStyle(Style.STYLE6);
+        holidays.setStyle(Style.STYLE7);
+
+        CalendarSource familyCalendarSource = new CalendarSource("Family");
+        familyCalendarSource.getCalendars().addAll(birthdays, holidays, tomi, javi, manolo, antonio, raul);
+
+        calendarView.getCalendarSources().setAll(familyCalendarSource);
+        calendarView.setRequestedTime(LocalTime.now());
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(calendarView); // introPane);
+
+        Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
+            @Override
+            public void run() {
+                while (true) {
+                    Platform.runLater(() -> {
+                        calendarView.setToday(LocalDate.now());
+                        calendarView.setTime(LocalTime.now());
+                    });
+
+                    try {
+                        // update every 10 seconds
+                        sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+
+        updateTimeThread.setPriority(Thread.MIN_PRIORITY);
+        updateTimeThread.setDaemon(true);
+        updateTimeThread.start();
+
+        Scene scene = new Scene(stackPane);
+        primaryStage.setTitle("Calendar");
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(1300);
+        primaryStage.setHeight(1000);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
